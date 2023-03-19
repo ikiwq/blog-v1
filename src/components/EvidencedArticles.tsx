c56cdc78321967c1765998252788e163ea4eab07
Iki <ikiigaiiii@gmail.com>
2023-03-19 14:44:59 +0100

    minor fixes

--- src/components/EvidencedArticles.tsx
+++ src/components/EvidencedArticles.tsx
@@ -17,21 +17,23 @@ const EvidencedArticles = (props: Props) => {
 
   return (
     <div className='flex flex-col md:grid grid-cols-1 xl:grid-cols-5 md:space-y-2 xl:space-y-0 xl:space-x-2'>
-        <div className='col-span-3 flex flex-col py-1 main-container overflow-hidden'>
+        <div className='col-span-3 flex flex-col py-1 main-container overflow-hidden max-h-80'>
                 <Link to={`/post/${props.posts[0].slug}`} className="">
-                  <img src={props.posts[0].featureImage?.url} className="h-72 w-full block object-cover hover:scale-105 transition duration-500"/>
+                  <img src={props.posts[0].featureImage?.url} className="h-80 w-full block object-cover hover:scale-105 transition duration-500"/>
                 </Link>
                 <Link to={`/post/${props.posts[0].slug}`}>
-                <div className='bg-neutral-900 text-white px-4 flex justify-between items-center py-3'>
+                <div className='bg-neutral-900 text-white px-4 flex flex-col h-28 justify-center w-full py-3'>
+                  <div className='flex justify-between'>
                     <h1 className='text-white font-semibold cursor-pointer text-xl hover:text-red-600 transition duration-500 text-left'>{props.posts[0].title}</h1>
                     <div className='flex items-center space-x-1 text-sm md:text-md xl:text-md text-red-600'>
                         <AiFillCalendar/>
                         <p className=' whitespace-nowrap'>{moment(props.posts[0].createdAt).format('MMM DD, YYYY')}</p>
                     </div>
-                </div>
-                <div className='bg-neutral-900 text-sm px-4 text-neutral-300 pb-2'>
+                  </div>
+                  <div className='bg-neutral-900 text-sm text-neutral-300 pb-2'>
                     <p>{props.posts[0].excerpt}</p>
-                </div>
+                  </div>  
+                </div>                
                 </Link>
         </div>
         <div className='col-span-2 md:grid grid-cols-2 grid-rows-2 gap-2 xl:grid-row-2 xl:grid-cols-1'>
